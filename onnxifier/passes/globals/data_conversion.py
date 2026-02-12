bb30b2f019bf4c5436b736e2a06a3d99e0aa5b91
Tang, Wenyi <wenyitang@outlook.com>
2026-02-12 19:09:56 +0800

    [fix] float_to_half works better (#44)

--- onnxifier/passes/globals/data_conversion.py
+++ onnxifier/passes/globals/data_conversion.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2025 The ONNXIFIER Authors.
+Copyright (C) 2026 The ONNXIFIER Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,15 +17,16 @@ limitations under the License.
 # pylint: disable=arguments-differ
 
 import itertools
-import re
+from typing import List
 
-import onnx
-from onnx import TensorProto
+from onnx import NodeProto, TensorProto
 from onnx.helper import make_attribute
 from onnx.numpy_helper import from_array, to_array
 
-from ... import OnnxGraph, logger
+from ... import OnnxGraph
 from .. import L1, PASSES
+from ..pattern import SingleNodePattern
+from ..rewriter import Rewriter
 from ..utils import cast_in
 
 
@@ -58,23 +59,19 @@ def half_to_float(graph: OnnxGraph) -> OnnxGraph:
     return graph
 
 
-def canonicalize_resize(graph: OnnxGraph, op_name: str):
+class CanonicalizeResizeScale(Rewriter):
     """The scales input of Resize must be float32."""
-    assert op_name in graph.nodes
-    node_pb = graph.nodes[op_name]["pb"]
-    scales = node_pb.input[2]
-    if scales:
-        graph.add_onnx_node(cast_in(node_pb, 2, TensorProto.FLOAT))
+
+    def __init__(self):
+        super().__init__(SingleNodePattern("Resize"))
+
+    def rewrite(self, graph: OnnxGraph, nodes: List[NodeProto]):
+        node = nodes[0]
+        if node.input[2]:
+            self += cast_in(node, 2, TensorProto.FLOAT)
 
 
-def dtype_canonicalize(graph: OnnxGraph, op_type: str, op_name: str):
-    """Canonicalize data type for operators that don't support fp16."""
-    if op_type == "Resize":
-        canonicalize_resize(graph, op_name)
-    # TODO: add more operators here
-
-
-@PASSES.register()
+@PASSES.register(patch=[CanonicalizeResizeScale])
 def float_to_half(graph: OnnxGraph) -> OnnxGraph:
     """Convert float32 consts and values to half."""
 
@@ -87,6 +84,11 @@ def float_to_half(graph: OnnxGraph) -> OnnxGraph:
                 attr = make_attribute(key="value", value=from_array(array))
                 node_pb.attribute.pop()
                 node_pb.attribute.append(attr)
+        elif node_pb.op_type == "Cast":
+            # if cast target is fp32, change it to fp16
+            for attr in node_pb.attribute:
+                if attr.name == "to" and attr.i == TensorProto.FLOAT:
+                    attr.i = TensorProto.FLOAT16
     for init in graph.initializer:
         if init.data_type == TensorProto.FLOAT:
             array = to_array(init).astype("float16")
@@ -95,12 +97,4 @@ def float_to_half(graph: OnnxGraph) -> OnnxGraph:
     for io in itertools.chain(graph.input, graph.output):
         if io.type.tensor_type.elem_type == TensorProto.FLOAT:
             io.type.tensor_type.elem_type = TensorProto.FLOAT16
-    # fix operators that don't support fp16
-    try:
-        onnx.checker.check_model(graph.model, full_check=True)
-    except onnx.shape_inference.InferenceError as e:
-        op_type = re.findall(r"op_type\:(\w+)", f"{e}")[0]
-        op_name = re.findall(r"node name\: (.*)\)", f"{e}")[0]
-        logger.debug(f"Operator {op_name}({op_type}) check failed, fixing it...")
-        dtype_canonicalize(graph, op_type, op_name)
     return graph
